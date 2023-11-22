82045b45e7a4adde955f17d73cb0bf049eab5467
LuksFP <149438171+LuksFP@users.noreply.github.com>
2023-11-22 07:50:16 -0300

    Add files via upload

---
+++ Aulateclado-main/cursojs1/aula001/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,14 @@
+function tocaSom(idElementoAudio)
+{document.querySelector(idElementoAudio).play();}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0; 
+
+while (contador < listaDeTeclas.length)  {
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador = contador + 1
+
+    console.log(contador);
+}
